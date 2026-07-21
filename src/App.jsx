8f1f5950e51d495d4ab755aab58ac0bb74d7bf23
MPatefield <m.patefield95@hotmail.co.uk>
2026-07-21 14:02:40 +0000

    Updated footer and some CSS elements (not yet finished)

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,9 @@ import Hero from './components/Hero'
 import About from './components/About'
 import Skills from './components/Skills'
 import Header from './components/Header'
+import Footer from './components/Footer'
+
+
 function App() {
 
 
@@ -12,6 +15,7 @@ function App() {
       <Hero />
       <About />
       <Skills />
+      <Footer />
     </div>
   )
 }
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,18 @@
 import React from "react";
-import'/.Footer.css';
+import './Footer.css';
 
 function Footer() {
     return(
-        <div>
-
-        </div>
+        <footer className="footer">
+            <p>&copy;{new Date().getFullYear()} Matthew James Patefield. All rights reserved.</p>
+            <div className="contact-buttons">
+            
+            <a href="mailto:m.patefield95@hotmail.co.uk">Email me</a>
+            <a href="https://github.com/MPatefield?tab=repositories">GitHub</a>
+            <a href="https://www.linkedin.com/in/matthew-patefield/">LinkedIn</a>
+            
+            </div>
+        </footer>
     )
 }
 
