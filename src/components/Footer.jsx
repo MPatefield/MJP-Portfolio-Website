50fac3fd071a569dace2bcc9befde234791f6802
MPatefield <m.patefield95@hotmail.co.uk>
2026-07-21 14:39:09 +0000

    Changing footer classnames as learnt about plain css imports not scoping from imports.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,7 @@ function Footer() {
         <footer className="footer text-center p-2">
             <p>&copy;{new Date().getFullYear()} Matthew James Patefield. All rights reserved.</p>
             <div className="contact-buttons">
-                <nav className="nav flex gap-10 justify-center p-2">
+                <nav className="footer-nav flex gap-10 justify-center p-2">
                     <a href="mailto:m.patefield95@hotmail.co.uk">Email me</a>
                     <a href="https://github.com/MPatefield?tab=repositories">GitHub</a>
                     <a href="https://www.linkedin.com/in/matthew-patefield/">LinkedIn</a>
