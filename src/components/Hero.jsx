fd10727c965798fe3b459de8f2ef490de96724fb
MPatefield <m.patefield95@hotmail.co.uk>
2026-07-21 09:44:52 +0000

    feat: add Hero section with Tailwind layout, custom colour variables, and fade-in animation

---
+++ src/components/Hero.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+import './Hero.css'
+
+function Hero() {
+    return(
+        <div className="hero-section flex flex-col items-center text-center py-20 mb-6">
+            <img src="/assets/profile.jpg" alt="Image of Matthew James Patefield"/>
+            <h1 className="hero-name">Matthew James Patefield</h1>
+            <p className="hero-tag">Developer in training..</p>
+        </div>
+    )
+}
+
+export default Hero
