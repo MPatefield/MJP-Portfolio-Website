d6a42437ddd2483b8d85d4eda4f7a50f9c0b8fe9
MPatefield <m.patefield95@hotmail.co.uk>
2026-07-21 14:25:17 +0000

    clear app.css

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,15 +2,15 @@ import React from "react";
 import './Footer.css';
 
 function Footer() {
-    return(
-        <footer className="footer">
+    return (
+        <footer className="footer text-center p-2">
             <p>&copy;{new Date().getFullYear()} Matthew James Patefield. All rights reserved.</p>
             <div className="contact-buttons">
-            
-            <a href="mailto:m.patefield95@hotmail.co.uk">Email me</a>
-            <a href="https://github.com/MPatefield?tab=repositories">GitHub</a>
-            <a href="https://www.linkedin.com/in/matthew-patefield/">LinkedIn</a>
-            
+                <nav className="nav flex gap-10 justify-center p-2">
+                    <a href="mailto:m.patefield95@hotmail.co.uk">Email me</a>
+                    <a href="https://github.com/MPatefield?tab=repositories">GitHub</a>
+                    <a href="https://www.linkedin.com/in/matthew-patefield/">LinkedIn</a>
+                </nav>
             </div>
         </footer>
     )
