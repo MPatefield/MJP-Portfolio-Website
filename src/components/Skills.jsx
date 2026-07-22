4c74378d0cdb5961ef7e17a12555d0c081cfed85
MPatefield <m.patefield95@hotmail.co.uk>
2026-07-22 17:01:12 +0000

    minor css tweaks

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -3,17 +3,17 @@ import './Skills.css'
 
 function Skills() {
     return(
-        <div id="skills" className="skills-section max-w-2xl mx-auto py-16 text-center">
+        <div id="skills" className="skills-section max-w-2xl mx-auto p-16 text-center">
             <h2 className="section-title mb-4">My Skills..</h2>
             <p>So I'm a self confessed newbie to coding/programming. Currently feel pretty confident in the below skills:</p>
-            <ul>
-                <li>HTML</li>
-                <li>CSS</li>
-                <li>Tailwind CSS</li>
-                <li>JavaScript</li>
-                <li>REACT</li>
+            <ul> 
+                <li>- HTML</li>
+                <li>- CSS</li>
+                <li>- Tailwind CSS</li>
+                <li>- JavaScript</li>
+                <li>- REACT</li>
             </ul>
-            <p>I'm hoping to further develop all the above and add more to my repertoire.</p>
+            <p className="mt-4">I'm hoping to further develop all the above and add more to my repertoire.</p>
         </div>
     )
 }
